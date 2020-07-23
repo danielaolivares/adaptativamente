76c0fd6107a963e6bf7a4226726385a5177b6796
danielaolivares <domioli@gmail.com>
2020-07-23 00:32:52 -0400

    i add index.js

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-document.getElementById("button")("click", ()=> {
-  console.log("se escucha")
-});
+document.getElementById("openModal").style.display= "none";
+ function condition() {
+  let rescuedName = document.getElementById("name").value;
+  let rescuedLastName = document.getElementById("lastName").value;
+  let rescuedEmail = document.getElementById("email").value;
+  let rescuedPhone = document.getElementById("phone").value;
+  let rescuedDescription = document.getElementById("description").value;
+  if (rescuedName==="") {
+    alert("No hay nombre");
+    return false
+  }if ( rescuedLastName==="") {
+    alert("Escribe tu apellido"); 
+    return false   
+  }if ( rescuedEmail==="") {
+    alert("Escribe tu mail");
+    return false   
+  } if ( rescuedPhone==="") {
+    alert("Escribe tu telefono");
+    return false  
+  }if ( rescuedDescription==="") {
+    alert("Falta descripcion");
+    return false  
+  } else {
+    document.getElementById("openModal").style.display= "block";
+      document.getElementById("showName").innerHTML=`<p>Nombre : ${rescuedName} <br>
+      Apellido : ${rescuedLastName} <br> Email : ${rescuedEmail} <br> Teléfono : ${rescuedPhone}  <br> Descripción : ${rescuedDescription}</p>`
+  }
+ }
